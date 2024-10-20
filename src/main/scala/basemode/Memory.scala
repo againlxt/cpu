7cc471ae633ed673bde77e58d9d1c384a9e4cf6f
23060306-Lei Xiao Tian <leixiaotian434@gmail.com>
2024-10-20 21:38:13 +0800

    modified:   .metals/metals.lock.db 	modified:   src/main/scala/basemode/Memory.scala

--- src/main/scala/basemode/Memory.scala
+++ src/main/scala/basemode/Memory.scala
@@ -50,3 +50,42 @@ class ReadWriteSmem(dataWidth: Int, addrWidth: Int, memorySize: Int) extends Mod
   } .otherwise {}
 }
 
+class RegistMem(dataWidth: Int, addrWidth: Int, memorySize: Int) extends Module {
+  val io = IO(new Bundle {} {
+    val enable  = Input(Bool())
+    val write   = Input(Bool())
+    val addr    = Input(UInt(addrWidth.W))
+    val baseAddr= Input(UInt(32.W))
+    val len     = Input(UInt(3.W))            // len 可以是 1, 2, 或 4
+    val dataIn  = Input(UInt(dataWidth.W))
+    val dataOut = Output(UInt(dataWidth.W))
+  })
+
+  val mem = RegInit(VecInit(Seq.fill(256)(0.U(32.W))))
+
+  io.dataOut := 0.U
+
+  val addrWire  = (io.addr - io.baseAddr) >> 2
+  val dataWire  = io.dataIn 
+
+  when(io.enable) {
+    when(io.write) {
+      when(io.len === 1.U) {
+        mem(addrWire) := dataWire(7, 0)
+      } .elsewhen (io.len === 2.U) {
+        mem(addrWire) := dataWire(15, 0)
+      } .elsewhen (io.len === 4.U) {
+        mem(addrWire) := dataWire(31, 0)
+      }
+    } .otherwise {
+      val memDataWire  = mem(addrWire)
+      when(io.len === 1.U) {
+        io.dataOut  := memDataWire(7, 0)
+      } .elsewhen (io.len === 2.U) {
+        io.dataOut  := memDataWire(15, 0)
+      } .elsewhen (io.len === 4.U) {
+        io.dataOut  := memDataWire(31, 0)
+      }
+    }
+  }
+}
