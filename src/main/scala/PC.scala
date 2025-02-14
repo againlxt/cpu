675733948cf70baca8da4340c58a1eccd4c6e96e
lxt <leixiaotian434@gmail.com>
2025-02-14 22:05:57 +0800

    modified:   src/main/scala/PC.scala

--- src/main/scala/PC.scala
+++ src/main/scala/PC.scala
@@ -11,8 +11,8 @@ class PC extends Module {
 		val pc			= Output(UInt(32.W))
 	})
 
-	/* 使用 RegInit 设置 PC 的初始值为 0x20000000 */
-	val pcReg = RegInit(BigInt("20000000", 16).U(32.W))
+	/* 使用 RegInit 设置 PC 的初始值为 0x30000000 */
+	val pcReg = RegInit(BigInt("30000000", 16).U(32.W))
 	val wbu2PCReadyReg = RegInit(1.U(1.W))
 	io.wbu2PC.ready := wbu2PCReadyReg
 
