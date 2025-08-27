5edae1f179262e29ba5f3f0b3b0dfa6fb670f352
LEI XIAO TIAN <leixiaotian434@gmail.com>
2025-08-27 16:02:00 +0800

    modified:   .bloop/root/root-analysis.bin 	modified:   src/main/scala/Config.scala

--- src/main/scala/Config.scala
+++ src/main/scala/Config.scala
@@ -23,10 +23,6 @@ object Config {
     def depthOfTable: Int = 8
     def offsetWidth:  Int = 2
     def tagWidth:     Int = 10
-<<<<<<< HEAD
-    def way:          Int = 4
-=======
     def way:          Int = 1
->>>>>>> pipeline
   }
 }
